d4f305a4dfa5b4afcb73c512180d760a98c453e4
Daksh Semwal <dsemwal2@gmail.com>
2020-09-29 18:05:48 +0530

    Init

--- app/src/main/java/app/kurosaki/developer/vegidel/ui/activities/WelcomeActivity.java
+++ app/src/main/java/app/kurosaki/developer/vegidel/ui/activities/WelcomeActivity.java
@@ -1,17 +1,26 @@
 package app.kurosaki.developer.vegidel.ui.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
 import app.kurosaki.developer.vegidel.R;
 import app.kurosaki.developer.vegidel.core.BaseActivity;
+import app.kurosaki.developer.vegidel.databinding.ActivityWelcomeBinding;
+import app.kurosaki.developer.vegidel.utils.Common;
 
 public class WelcomeActivity extends BaseActivity {
+
+    ActivityWelcomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_welcome);
+        initView();
+    }
+
+    private void initView() {
     }
 }
